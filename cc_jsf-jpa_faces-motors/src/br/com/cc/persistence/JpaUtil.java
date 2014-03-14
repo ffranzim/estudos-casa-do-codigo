0dfde2a502c34b932d22a9fd61ea77f34d9b21e1
fernando.franzim <fernando.franzim@gmail.com>
2014-03-14 05:27:16 +0000

    Alteração de nomenclatura

---
+++ cc_jsf-jpa_faces-motors/src/br/com/cc/persistence/JpaUtil.java
@@ -0,0 +1,55 @@
+package br.com.cc.persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import org.hibernate.Session;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class JpaUtil {
+
+	private static ThreadLocal<EntityManager> manager = new ThreadLocal<EntityManager>();
+
+	private static EntityManagerFactory factory;
+
+	public static boolean isEntityManagerOpen() {
+		return JpaUtil.manager.get() != null && JpaUtil.manager.get().isOpen();
+	}
+
+	public static EntityManager getEntityManager() {
+		if (JpaUtil.factory == null) {
+			JpaUtil.factory = Persistence.createEntityManagerFactory("facesMotorsPU");
+		}
+		EntityManager em = JpaUtil.manager.get();
+		if (em == null || !em.isOpen()) {
+			em = JpaUtil.factory.createEntityManager();
+			JpaUtil.manager.set(em);
+		}
+		return em;
+	}
+	
+	public static void evictCache(EntityManager em, String region){
+		((Session)em.getDelegate()).getSessionFactory().getCache().evictQueryRegion(region);
+	}
+
+	public static void closeEntityManager() {
+		EntityManager em = JpaUtil.manager.get();
+		if (em != null) {
+			EntityTransaction tx = em.getTransaction();
+			if (tx.isActive()) {
+				tx.commit();
+			}
+			em.close();
+			JpaUtil.manager.set(null);
+		}
+	}
+
+	public static void closeEntityManagerFactory() {
+		closeEntityManager();
+		JpaUtil.factory.close();
+	}
+}
