ccfb45282e1d154f12e081d18f6c17a5012dbf16
fernando.franzim <fernando.franzim@gmail.com>
2015-01-14 01:21:17 +0000

    Coisas novas

--- cc_jsf-jpa_faces-motors/src/br/com/cc/mb/MarcaBean.java
+++ cc_jsf-jpa_faces-motors/src/br/com/cc/mb/MarcaBean.java
@@ -1,20 +1,32 @@
 package br.com.cc.mb;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.persistence.EntityManager;
 
 import lombok.Getter;
 import br.com.cc.entity.Marca;
 import br.com.cc.persistence.JpaUtil;
 
+@ViewScoped
 @ManagedBean
 @Getter
 public class MarcaBean {
 
 	private List<Marca> marcas;
+
+	private List<Marca> marcasParaIncluir = new ArrayList<Marca>();
+	
+	private Marca novaMarca = new Marca();
+
+	public void addMarca() {
+		marcasParaIncluir.add(novaMarca);
+		this.novaMarca = new Marca();
+	}
 
 	@PostConstruct
 	public void carregaMarcas() {
