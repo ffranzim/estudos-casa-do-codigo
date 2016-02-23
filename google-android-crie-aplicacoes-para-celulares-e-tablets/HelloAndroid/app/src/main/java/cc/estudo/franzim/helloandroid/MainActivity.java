ed6576f75bfc9f088cadd01a3fb65a74d4b804eb
Fernando Franzim <fernando.franzim@gmail.com>
2016-02-23 20:19:39 -0300

    Iniciando estudos android

---
+++ google-android-crie-aplicacoes-para-celulares-e-tablets/HelloAndroid/app/src/main/java/cc/estudo/franzim/helloandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package cc.estudo.franzim.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by franzim on 22/02/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
